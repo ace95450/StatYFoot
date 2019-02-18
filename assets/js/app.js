8c63e3d1e16863188724a2fa6966641e9c1685a0
brikema <brice_ma@hotmail.fr>
2019-02-18 15:02:48 +0100

    CSS et HTML Ok
    Page Détail à finir en html

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,8 +5,16 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
+// any CSS you require will output into a single css file (app.scss in this case)
 require('../css/app.css');
+
+/ -- Chargement du CSS
+require('./css/style.scss');
+// -- Chargement du JS
+require('./js/jquery.min');
+require('./js/bootstrap.js');
+require('./js/owl.carousel.min.js');
+require('./js/main.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
