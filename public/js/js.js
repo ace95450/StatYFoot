68f43f210f8fa6b8a8d3b73f38d36881f3e3e1e2
brikema <brice_ma@hotmail.fr>
2019-02-24 22:58:14 +0100

    C'est un peu beau. Rin de neuf dans l'api

--- public/js/js.js
+++ public/js/js.js
@@ -17,3 +17,23 @@ function searchBar() {
         }
     }
 }
+
+function searchBarligue() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('a');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("div")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
